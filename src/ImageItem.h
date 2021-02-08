935fc2fc516070d6a6acf6e7c72fb2c440e95304
liuyangping <liuyangping207@qq.com>
2021-02-08 18:02:56 +0800

    initialize

--- src/ImageItem.h
+++ src/ImageItem.h
@@ -8,18 +8,58 @@
 #include <CVEX/CVEX_Context.h>
 #include <memory>
 #include <vector>
-struct ImageItem {
+#include <iostream>
 
-    void bindItem(ImageItem *item){
+
+struct ImageItem {
+    ImageItem () = default;
+    ImageItem(int w, int h, CVEX_Type outputDataType = CVEX_TYPE_VECTOR3) :
+            width(w),
+            height(h),
+            outputType(outputDataType) {
+    }
+
+    void bindItem(ImageItem *item) {
         bindImageItems.emplace_back(item);
     }
 
-    std::vector<ImageItem*> bindImageItems;
-    bool isCompiled = false;
-    std::unique_ptr<CVEX_Context> CVEXPtr= std::make_unique<CVEX_Context>() ;
-    CVEX_Value *inputImageData;
-    CVEX_Value *outputImageData;
+    void setupInputArguments(){
 
+    }
+
+
+
+
+    // before compile the VEX compiler should make sure the right number of arguments
+    void compileVEXFile(const char *file) {
+        char *argv[2] = {nullptr, nullptr};
+        strcpy(argv[0], file);
+        if (!CVEXPtr->load(0, argv)) {
+            std::cout << "Error loading vex:" << argv[0] << std::endl;
+        } else isCompiled = true;
+
+    }
+
+
+    bool isCompiled = false;
+    std::unique_ptr<CVEX_Context> CVEXPtr = std::make_unique<CVEX_Context>();
+
+
+    std::vector<CVEX_Value *> uvCoord;            // system internal data, name : "uv" , type: CVEX_TYPE_VECTOR2
+    std::vector<CVEX_Value *> inputListImageData; // support multiple input arguments
+    CVEX_Value *outputImageData;        //may be float,vector, vector4, we only maintain one output, even VEX support >=1 output arguments
+
+    // bind other node
+
+    std::vector<ImageItem *> bindImageItems;
+
+
+    // width & height of this buffer
+    int width = 500;
+    int height = 500;
+
+    // Image Type of output
+    CVEX_Type outputType;
 };
 
 
